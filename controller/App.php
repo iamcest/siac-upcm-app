968e106ca097373e6b2555a910705d242ce31481
roineradrianza <roineradrianzap@gmail.com>
2020-12-17 10:44:28 -0400

    Librería de encriptación añadida [php-encryption]

--- controller/App.php
+++ controller/App.php
@@ -3,6 +3,7 @@
  * 
  */
 if (session_status() === PHP_SESSION_NONE) session_start();
+require_once("vendor/autoload.php");
 require_once("models/helper/DB.php");
 require_once("controller/Helper.php");
 require_once("controller/Routes.php");
@@ -12,5 +13,4 @@ class App
 	function __construct() {
 		new Routes();
 	}
-
 }
