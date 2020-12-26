5a18fca5a58b54e96e1f92ccd40f03fc12a87177
roineradrianza <roineradrianzap@gmail.com>
2020-12-26 13:32:25 -0400

    Sección algoritmos actualizada[dyslipedimia-risk-treatment] a la espera de último algoritmo

--- views/algorithms/dyslipedimia-risk-treatment.php
+++ views/algorithms/dyslipedimia-risk-treatment.php
@@ -14,18 +14,31 @@
           <v-form>
             <v-row class="px-16 mx-auto">
 
-              <v-col cols="12" md="12">
+              <v-col cols="12" md="6">
                 <label>c-LDL</label>
-                <v-text-field class="mt-3" v-model="calc.amount" hint="Ingrese la unidad en mg/dl" suffix="mg/dl" outlined required></v-text-field>
+                <v-text-field class="mt-3" v-model="c_ldl" hint="Ingrese la unidad en mg/dl" suffix="mg/dl" outlined required></v-text-field>
+              </v-col>   
+
+              <v-col cols="12" md="6">
+                <label>Triglicéridos</label>
+                <v-text-field class="mt-3" v-model="tg" hint="Ingrese la unidad en mg/dl" suffix="mg/dl" outlined required></v-text-field>
+              </v-col>   
+
+              <v-col cols="12" md="6">
+                <label>c-No HDL</label>
+                <v-text-field class="mt-3" v-model="c_no_hdl" hint="Ingrese la unidad en mg/dl" suffix="mg/dl" outlined required></v-text-field>
               </v-col>   
 
               <v-col cols="12" md="4" offset-md="4">
-                <label class="text-center result-label">Resultado</label>
-                <v-text-field class="mt-3 result-box" outlined readonly required ></v-text-field>
-              </v-col>  
-              <v-col cols="12">
+                <label class="text-center d-flex justify-center">Diagnóstico</label>
+                <v-text-field class="mt-3 result-box" :value="diagnostic" outlined readonly required ></v-text-field>
+              </v-col>
+              <v-col cols="12" v-if="treatment.active">
                 <h3 class="mb-4 grey--text text--darken-2">Tratamiento</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum iure doloribus, minima saepe quos nulla, velit! Enim, beatae maiores saepe a, dolorum voluptas nesciunt obcaecati sit neque vel, omnis fugiat!</p>
+                <p v-if="treatment !== ''">{{ treatment.text }}</p>                
+              </v-col>
+              <v-col cols="12" md="6" v-if="pcsk9_item">
+                <h4>Inhibidores de la PCSK9</h4>
                 <v-list-item class="pl-0">
                   <v-list-item-content class="grey--text text--darken-1">
                     <v-list-item-title>RRA: 1,6</v-list-item-title>
@@ -36,12 +49,33 @@
                     <v-list-item-title>RRR: 24%</v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>                
+              </v-col>
+              <v-col cols="12" md="6" v-if="ezt_item">
+                <h4>EZT</h4>
                 <v-list-item class="pl-0">
                   <v-list-item-content class="grey--text text--darken-1">
-                    <v-list-item-title>RRR: 65</v-list-item-title>
+                    <v-list-item-title>RRA: 2.0</v-list-item-title>
                   </v-list-item-content>
-                </v-list-item>
+                </v-list-item>                
+                <v-list-item class="pl-0">
+                  <v-list-item-content class="grey--text text--darken-1">
+                    <v-list-item-title>RRR: 6.4%</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>            
               </v-col>
+              <v-col cols="12" md="6" v-if="ao3_fibratos_item">
+                <h4>AO3 / Fibratos</h4>
+                <v-list-item class="pl-0">
+                  <v-list-item-content class="grey--text text--darken-1">
+                    <v-list-item-title>RRA: 2.8</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>                
+                <v-list-item class="pl-0">
+                  <v-list-item-content class="grey--text text--darken-1">
+                    <v-list-item-title>RRR: 24.8%</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>            
+              </v-col>     
             </v-row>
           </v-form>
         </v-col>
--- views/assets/js/algorithms/dyslipedimia-risk-treatment.js
+++ views/assets/js/algorithms/dyslipedimia-risk-treatment.js
@@ -4,13 +4,40 @@ let vm = new Vue({
     el: '#siac-suite-container',
     data: {
       loading: false,
-      calc: {
-        height: 0.00,
-        weight: 0.00
+      pcsk9_item: false,
+      ezt_item: false,
+      ao3_fibratos_item: false,
+      treatment: {
+        active: false,
+        text: '',
       },
+      diagnostic: '',
+      c_ldl: '',
+      c_no_hdl: '',
+      tg: '',
+    },
+
+    watch: {
+      c_ldl: {
+        handler: 'calc'
+      },
+      c_no_hdl: {
+        handler: 'calc'
+      },
+      tg: {
+        handler: 'calc'
+      }
     },
 
     computed: {
+      md_width () {
+        if (this.c_ldl >= 70) {
+          return 6
+        }
+        else {
+          return 12
+        }
+      },
     },
 
     created () {
@@ -20,7 +47,47 @@ let vm = new Vue({
     },
 
     methods: {
-      reserve () {
+      calc () {
+        var app = this
+        var c_ldl = parseInt(this.c_ldl)
+        var c_no_hdl = parseInt(this.c_no_hdl)
+        var tg = parseInt(this.tg)
+
+        app.diagnostic = ''
+        app.treatment.active = false
+        app.treatment.text = ''
+        app.ao3_fibratos_item = false
+        app.ezt_item = false
+        app.pcsk9_item = false
+
+        if (c_ldl >= 70) {
+          if (tg >= 200 && c_no_hdl >= 100) {
+            app.treatment.active = true
+            app.ao3_fibratos_item = true
+          }
+          else {
+            if (tg < 200 && c_no_hdl < 100) {
+              app.treatment.active = true
+              app.pcsk9_item = true    
+              app.ezt_item = true
+            }
+          }
+        }
+        else if (c_ldl <= 50) {
+          app.diagnostic = 'óptimo'
+        }
+        else if (c_ldl > 50 && c_ldl <= 70) {
+          if (tg >= 200 && c_no_hdl >= 100) {
+            app.treatment.active = true
+            app.ao3_fibratos_item = true           
+          }
+          else{
+            if (tg > 0 && c_no_hdl > 0) {
+              app.treatment.active = true
+              app.pcsk9_item = true
+            }
+          }
+        }
       },
     }
 });
