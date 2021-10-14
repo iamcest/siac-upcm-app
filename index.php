498b00b90358369cb6f3d54b49130b7d196fc416
Roiner Adrianza <roineradrianzap@gmail.com>
2021-10-14 19:03:12 -0400

    Manejador de errores php desactivado

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
 require "vendor/autoload.php";
-
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
-
 require "controller/App.php";
 
 new App();
--- views/parts/upcm_logo.php
+++ views/parts/upcm_logo.php
@@ -1,10 +1,10 @@
+<?php if (!empty($_SESSION['upcm_logo'])) : ?>
+<v-row>
+    <v-col class="d-flex justify-end px-8" cols="12">
+        <v-img src="<?php echo SITE_URL ?>/public/img/upcms-logo/<?php echo $_SESSION['upcm_logo'] ?>" max-width="100px"
+            max-height="100px">
 
-		<?php if ($_SESSION['upcm_logo'] != null || $_SESSION['upcm_logo'] != ''): ?>
-		<v-row>
-			<v-col class="d-flex justify-end px-8" cols="12">
-				<v-img src="<?php echo SITE_URL ?>/public/img/upcms-logo/<?php echo $_SESSION['upcm_logo'] ?>" max-width="100px" max-height="100px">
-					
-				</v-img>
-			</v-col>
-		</v-row>
-		<?php endif ?>
+        </v-img>
+    </v-col>
+</v-row>
+<?php endif ?>
